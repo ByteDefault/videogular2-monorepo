c927ec543a686c29ebe8a6c3ff261f2cbe6493b3
Jorge Cano <jorgeucano@gmail.com>
2018-11-13 09:59:36 -0800

    feat(update): add buffering module

---
+++ projects/videogular/buffering/src/lib/buffering.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { VgBuffering } from './vg-buffering';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [VgBuffering],
+  exports: [VgBuffering]
+})
+export class BufferingModule { }
---
+++ projects/videogular/buffering/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of buffering
+ */
+
+export * from './lib/buffering.service';
+export * from './lib/buffering.component';
+export * from './lib/buffering.module';
